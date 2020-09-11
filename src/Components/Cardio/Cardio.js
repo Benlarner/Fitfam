d133f93e5fb00e2a2767604d7b79d5f137d14778
Benlarner <stroudwoodfitness@gmail.com>
2020-09-11 09:44:11 -0400

    folder structure made

---
+++ src/Components/Cardio/Cardio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Cardio.scss';
+import {Link} from 'react-router-dom';
+
+class Cardio extends React.Component {
+
+    state = {
+        cardioData: []
+    }
+
+    render() {
+
+        return(
+
+            <section className = "cardio">
+
+            </section>
+        )
+    }
+
+}
